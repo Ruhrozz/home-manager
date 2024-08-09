e4b46250c39660c45ce1301f651c7e7cc1fe1444
Ruhrozz <ruhrozz@bk.ru>
2024-08-09 19:31:11 +0800

    chore: flake update

--- shell/default.nix
+++ shell/default.nix
@@ -3,9 +3,11 @@
 {
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
+    enableCompletion = false;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+
+    sessionVariables = { ZSH_DISABLE_COMPFIX = true; };
 
     shellAliases = {
       ll = "ls -l";
@@ -35,5 +37,20 @@
       plugins = [ "git" ];
       theme = "robbyrussell";
     };
+
+    initExtra = ''
+        # >>> mamba initialize >>>
+        # !! Contents within this block are managed by 'mamba init' !!
+        export MAMBA_EXE='/nix/store/80pl4hvrirdam4g4y39dv1plw9q6kf95-micromamba-1.5.8/bin/micromamba';
+        export MAMBA_ROOT_PREFIX='/home/malofeev/micromamba';
+        __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+        if [ $? -eq 0 ]; then
+            eval "$__mamba_setup"
+        else
+            alias micromamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+        fi
+        unset __mamba_setup
+        # <<< mamba initialize <<<
+    '';
   };
 }
