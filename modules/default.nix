25894ae74a5da0fb4e34942c8491272d771749bd
Ruhrozz <ruhrozz@bk.ru>
2024-09-19 11:32:03 +0700

    feat(tmux): add tmux config

--- modules/default.nix
+++ modules/default.nix
@@ -1,5 +1,5 @@
 {
-  imports = [ ./eza.nix ./git.nix ];
+  imports = [ ./eza.nix ./git.nix ./tmux.nix ];
 
   programs.thefuck.enable = true; # fix mistakes promptly
   programs.bat.enable = true; # `cat` enhanced
---
+++ modules/tmux.nix
@@ -0,0 +1,6 @@
+{
+  programs.tmux = {
+    enable = true;
+    mouse = true;
+  };
+}
