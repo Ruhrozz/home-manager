bf41e7cda950715b6858096caf9b33426a02272d
Ruhrozz <ruhrozz@bk.ru>
2024-09-19 11:17:10 +0700

    fix(micromamba): adaptive mamba root path for any user

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -72,7 +72,7 @@
       # >>> mamba initialize >>>
       # !! Contents within this block are managed by 'mamba init' !!
       export MAMBA_EXE=`which micromamba`;
-      export MAMBA_ROOT_PREFIX='/home/malofeev/micromamba';
+      export MAMBA_ROOT_PREFIX="$HOME/micromamba";
       __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
       if [ $? -eq 0 ]; then
           eval "$__mamba_setup"
