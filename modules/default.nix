d560b087fea504ef596fd4dd7fa235c704fde0e6
Ruhrozz <ruhrozz@bk.ru>
2024-09-21 17:24:15 +0700

    feat(htop): add htop to modules

--- modules/default.nix
+++ modules/default.nix
@@ -1,5 +1,7 @@
+{ ... }:
+
 {
-  imports = [ ./eza.nix ./git.nix ./tmux.nix ];
+  imports = [ ./eza.nix ./git.nix ./tmux.nix ./htop.nix ];
 
   programs.thefuck.enable = true; # fix mistakes promptly
   programs.bat.enable = true; # `cat` enhanced
